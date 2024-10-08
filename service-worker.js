76edb5bd23231926a730ff79b7af8148fcec432e
Akutangulo <akutangulo@gmail.com>
2024-10-08 20:23:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     // Aquí puedes agregar cualquier acción que quieras que ocurra cuando se haga clic en la notificación
 });
-
 self.addEventListener('push', function(event) {
     const options = {
         body: 'El remontado ha terminado.',
